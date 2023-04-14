24fc499a6e24ba868785f307ceb4493b118d2ca3
loezsilva <luizinho545@gmail.com>
2023-04-14 17:39:14 -0300

    Alterações no settings

--- m7/settings.py
+++ m7/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import Csv, config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=%d&k=2k!m!n^p&tta%i=%_4o-z_=gp!sc6kshg^#&x(0=dr6h'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default=[], cast=Csv())
 
 
 # Application definition
@@ -142,3 +141,6 @@ LOGIN_REDIRECT_URL = 'core:dashboard'
 USE_L10N = False
 USE_THOUSAND_SEPARATOR = True
 DECIMAL_SEPARATOR=','
+
+
+
