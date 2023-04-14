d16f6533b1f2cb240db62d021e301835ebae5816
franklindias <franklindias99@gmail.com>
2023-04-14 17:36:02 -0300

    Ajustes no settings

--- m7/settings.py
+++ m7/settings.py
@@ -23,6 +23,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default=[], cast=Csv())
 
+
 INSTALLED_APPS = [
     'admin_interface',
     'colorfield',
