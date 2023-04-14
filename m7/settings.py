152fdade66d6ddf8419fdfdd18a5b419a52400e0
loezsilva <luizinho545@gmail.com>
2023-04-14 17:30:28 -0300

    Ajustes

--- m7/settings.py
+++ m7/settings.py
@@ -127,10 +127,10 @@ USE_TZ = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 
 STATIC_LOCATION = 'static'
 
